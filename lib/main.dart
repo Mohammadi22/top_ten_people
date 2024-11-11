19a96291220e30e3cce4ac4b081abd8c68f325bc
Abdullah <abdullahmh047@gmail.com>
2024-11-12 01:30:11 +0430

    Adding the home page for the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:top_ten_rich_people_in_world/Home.dart';
 
-void main() {
-  runApp(MaterialApp(home:  MyApp()));
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: SplashScreen(),
+  ));
 }
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class SplashScreen extends StatefulWidget {
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context,  MaterialPageRoute(builder: (context)=> Home() ),);
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: 
+      Center(child: Text("Logo",style: TextStyle(fontSize: 40),),),
+    ) ;
   }
 }
